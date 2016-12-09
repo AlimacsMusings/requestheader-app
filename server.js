5c438d19b89967e17d00b112e7677149ccf58b42
alimacsmusings <almcgreavy@gmail.com>
2016-12-09 12:00:45 +0000

    Working version set to port 8080 for Cloud9

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-
+var requestIp = require("request-ip");
+var osName= require("os-name");
 var express = require('express');
 var app = express();
 
-
 app.get('/', function(req, res) {
-     console.log("In get");
-     res.send("Hello World!");   
+     
+     //IP address
+     var clientIp = requestIp.getClientIp(req); 
+     //console.log("IP " + clientIp);
+     
+     //Language
+     var lang = req.headers["accept-language"].slice(0,5);
+     //console.log("Lang " + lang);
+
+     //Operating system
+     var opsys = osName();
+     //console.log("Ops " + opsys);
+     
+     var str = JSON.stringify({'ipaddress' : clientIp,
+                              'language' : lang,
+                              'software' : opsys});
+     //console.log(str);                         
+     
+     res.send(str);   
 });
 
 
 app.listen(8080, function(){
-	console.log('RequestHeader app listening on port 8080')
+	console.log('RequestHeader app listening on port 8080');
 });
